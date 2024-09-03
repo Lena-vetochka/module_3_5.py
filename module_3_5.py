6db2ebee4321acea0a26ef8f5c804d5eb98819c9
Lena-vetochka <elenanechaeva1701@yandex.ru>
2024-09-04 00:25:11 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -5,7 +5,7 @@ def get_multiplied_digits (number):
     first = int(str_number [0])
     if len(str_number) == 1:
         return first
-    if len(str_number) > 1:
+    else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
 
